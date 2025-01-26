f7330a76d54663629d9310be175cc5ab16b9062b
gordosaurio <63026665+gordosaurio@users.noreply.github.com>
2025-01-26 17:57:54 -0500

    update

---
+++ prueba-fronted/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <div className="background-image">
+      </div>
+    </div>
+  )
+}
+
+export default App
