6258654ec934233c8459c048e24335a749340097
gordosaurio <63026665+gordosaurio@users.noreply.github.com>
2025-01-26 19:18:07 -0500

    UPDATE

--- prueba-fronted/src/App.jsx
+++ prueba-fronted/src/App.jsx
@@ -1,11 +1,39 @@
-
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/get-categories/') // El prefijo '/api' activa el proxy en Vite
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.error('Error fetching categories:', error));
+  }, []);
+  
+  
+
 
   return (
     <div className="app-container">
-      <div className="background-image">
+      <div className="background-image"></div>
+      <div className="cards-container">
+        {categories.map((category) => (
+          <div
+            key={category.id}
+            className="card"
+            style={{
+              backgroundImage: `url(https://api.test.interactiva.net.co${category.image_banner})`,
+            }}
+          >
+            <div className="card-text">{category.name}</div>
+          </div>
+        ))}
       </div>
     </div>
   )
--- prueba-fronted/vite.config.js
+++ prueba-fronted/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.test.interactiva.net.co', // URL de tu API
+        changeOrigin: true,
+        secure: false, // Usa `false` si la API usa HTTPS con un certificado autofirmado
+      },
+    },
+  },
 })
